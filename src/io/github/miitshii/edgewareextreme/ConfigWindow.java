b78139eb28057642adca02b9812d1df0ea18f2fa
Miitshii <91094050+Miitshii@users.noreply.github.com>
2021-09-20 23:32:18 +0200

    Added Tray Icon

--- src/io/github/miitshii/edgewareextreme/ConfigWindow.java
+++ src/io/github/miitshii/edgewareextreme/ConfigWindow.java
@@ -1,7 +1,12 @@
 package io.github.miitshii.edgewareextreme;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class ConfigWindow extends JFrame {
 
@@ -14,6 +19,32 @@ public class ConfigWindow extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         setVisible(true);
+
+        setupSystemTray();
+    }
+
+    private void setupSystemTray() {
+        if (!SystemTray.isSupported()) {
+            return;
+        }
+
+        // create icon and set it
+        try {
+            TrayIcon trayIcon = new TrayIcon(ImageIO.read(getClass().getResource("/resources/images/smiling-face-with-horns_1f608.png")), "EdgewareExtreme");
+            trayIcon.setImageAutoSize(true);
+            trayIcon.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent ev) {
+                    if (ev.getClickCount() == 2) {
+                        setVisible(true);
+                    }
+                }
+            });
+            SystemTray systemTray = SystemTray.getSystemTray();
+            systemTray.add(trayIcon);
+        } catch (AWTException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
