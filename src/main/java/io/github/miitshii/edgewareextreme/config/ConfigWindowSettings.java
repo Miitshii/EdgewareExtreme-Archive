16d73afceafb2ee402d4141deffa1d80b70e648f
Miitshii <91094050+Miitshii@users.noreply.github.com>
2021-09-21 20:23:28 +0200

    Fixed layout issues

--- src/main/java/io/github/miitshii/edgewareextreme/config/ConfigWindowSettings.java
+++ src/main/java/io/github/miitshii/edgewareextreme/config/ConfigWindowSettings.java
@@ -1,20 +1,23 @@
 package io.github.miitshii.edgewareextreme.config;
 
+import io.github.miitshii.edgewareextreme.settings.BasicGBC;
 import io.github.miitshii.edgewareextreme.settings.GsonSettings;
 import io.github.miitshii.edgewareextreme.settings.SettingsSlider;
 
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class ConfigWindowSettings extends JPanel {
 
+    private GridBagLayout layout;
+
     public ConfigWindowSettings() {
         setBorder(new EmptyBorder(10, 10, 10, 10));
-        setLayout(new GridLayout(0, 1, 10, 10));
+        setLayout(layout = new GridBagLayout());
 
-        new GsonSettings();
         setupAnnoyanceSettings();
     }
 
@@ -22,13 +25,12 @@ public class ConfigWindowSettings extends JPanel {
         JPanel annoyanceSettings = new JPanel();
         GridBagLayout gbl = new GridBagLayout();
         annoyanceSettings.setLayout(gbl);
-        annoyanceSettings.setBorder(new TitledBorder(BorderFactory.createLineBorder(UIManager.getColor("Borders.color")), "Annoyance"));
+        annoyanceSettings.setBorder(new CompoundBorder(new TitledBorder(BorderFactory.createLineBorder(UIManager.getColor("Borders.color")), "Annoyance"), new EmptyBorder(5, 5, 5, 5)));
 
         new SettingsSlider(GsonSettings.M::getAnnoyanceDelay, GsonSettings.M::setAnnoyanceDelay, "Time Delay (ms)", 10, 60000, annoyanceSettings, gbl, 0);
-        new SettingsSlider(GsonSettings.M::getAnnoyanceDelay, GsonSettings.M::setAnnoyanceDelay, "Time Daaelay (ms)", 10, 60000, annoyanceSettings, gbl, 1);
-        new SettingsSlider(GsonSettings.M::getAnnoyanceDelay, GsonSettings.M::setAnnoyanceDelay, "Time Daaaaaaaaaaaaaaaelay (ms)", 10, 60000, annoyanceSettings, gbl, 2);
 
         add(annoyanceSettings);
+        layout.setConstraints(annoyanceSettings, new BasicGBC(0, 1, 1, 1, 1, 0, GridBagConstraints.HORIZONTAL, GridBagConstraints.NORTH));
     }
 
 }
